3623f24e32c98d0c1ea3093dff5693a4f4a75a10
dzkmrn <2141720076@student.polinema.ac.id>
2023-02-22 09:09:28 +0700

    kode kedua katanye

---
+++ 02_praktikum_web_lanjut_satu/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    echo "Selamat Datang!";
+});
+
+Route::get('/about', function () {
+    echo "Nama: Muhammad Dzaka Murran Rusid\n";
+    echo "NIM: 2141720076";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman artikel dengan ID " . $id;
+}); 
