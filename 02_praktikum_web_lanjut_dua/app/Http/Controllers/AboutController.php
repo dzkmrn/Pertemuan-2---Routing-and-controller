a22990e73fc735bf71da9331d290e24709a0a4e9
dzkmrn <2141720076@student.polinema.ac.id>
2023-02-22 09:50:20 +0700

    perubahan kedua

---
+++ 02_praktikum_web_lanjut_dua/app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return "Nama: Muhammad Dzaka Murran Rusid\nNIM: 2141720076";
+    }
+}
--- 02_praktikum_web_lanjut_dua/routes/web.php
+++ 02_praktikum_web_lanjut_dua/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;  
+use App\Http\HomeController;
+use App\Http\AboutController;  
+use App\Http\ArticleController;  
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,8 +16,8 @@ use App\Http\Controllers\PageController;
 |
 */
 
-Route::get('/', [PageController::class,'index']);
+Route::get('/', [HomeController::class,'index']);
 
-Route::get('/about', [PageController::class,'about']);
+Route::get('/about', [AboutController::class,'about']);
 
-Route::get('/articles/{id}', [PageController::class,'articles']);
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
